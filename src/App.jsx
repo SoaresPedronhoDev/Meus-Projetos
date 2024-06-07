c6e3cbafd8649c8c93c30667e68f10dd914670dd
Pedro Henrique <pedrohenriquedevprog@gmail.com>
2024-06-06 21:50:31 -0300

    adicionado esquema de cores do contact em diferentes dispositivos

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import SearchHeader from './assets/components/searchHeader';
 import Header from './assets/Header/header';
 import About from './assets/About.jsx/About';
 import Diplomas from './assets/Diplomas/diplomas';
-import ContactMe from './assets/components/contactMe';
+import ContactMe from './assets/components/contactMe.jsx';
 import MySwiper from './assets/components/swiperDiplomas';
 import Projetos from './assets/Projetos/projetos';
 import './App.css'
--- src/assets/components/contactMe.jsx
+++ src/assets/components/contactMe.jsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import '../Styles/contactStyle.css'
+import '../Styles/contactStyle.css';
 
 const ContactMe = () => {
     const [scrollPosition, setScrollPosition] = useState(0);
     const [color, setColor] = useState('black');
+    const [deviceType, setDeviceType] = useState('');
 
     useEffect(() => {
         const handleScroll = () => {
             const position = window.scrollY;
             setScrollPosition(position);
-            };
+            console.log(position)
+        };
 
         window.addEventListener('scroll', handleScroll);
 
@@ -19,28 +21,128 @@ const ContactMe = () => {
     }, []);
 
     useEffect(() => {
-        const scrollThreshold = 60;
-        const scrollThreshold2 = 820;
-        const scrollThreshold3 = 2256;
+        const determineDeviceType = () => {
+            let newDeviceType = '';
 
-        let color;
+            if (window.innerWidth <= 767) {
+                newDeviceType = 'mobile';
+            } else if (window.innerWidth <= 991) {
+                newDeviceType = 'tablet-small';
+            } else if (window.innerWidth <= 1199) {
+                newDeviceType = 'tablet';
+            } else if (window.innerWidth <= 1399) {
+                newDeviceType = 'notebook';
+            } else {
+                newDeviceType = 'desktop';
+            }
 
-        if(scrollPosition > scrollThreshold3){
-            color = 'black';
-        }else if(scrollPosition > scrollThreshold2){
-            color = 'white';
-        }else if(scrollPosition > scrollThreshold){
-            color = 'black';
-        } else{
-            color = "white"
+            setDeviceType(newDeviceType);
+        };
+
+        determineDeviceType();
+
+        window.addEventListener('resize', determineDeviceType);
+
+        return () => {
+            window.removeEventListener('resize', determineDeviceType);
+        };
+    }, []);
+
+    useEffect(() => {
+        let newColor;
+
+        if (deviceType === 'mobile') {
+            const scrollThresholdMobile = 50;
+            const scrollThresholdMobile2 = 821;
+            const scrollThresholdMobile3 = 2352;
+
+            if (scrollPosition > scrollThresholdMobile3) {
+                newColor = 'black';
+            } else if (scrollPosition > scrollThresholdMobile2) {
+                newColor = 'white';
+            } else if (scrollPosition > scrollThresholdMobile) {
+                newColor = 'black';
+            } else {
+                newColor = 'white';
+            }
+        } 
+
+        if(deviceType === 'tablet-small'){
+
+            const scrollThresholdTabletSmall = 67
+            const scrollThresholdTabletSmall2 = 1060
+            const scrollThresholdTabletSmall3 = 3040
+
+            if(scrollPosition > scrollThresholdTabletSmall3){
+                newColor = 'black'
+            }else if(scrollPosition > scrollThresholdTabletSmall2){
+                newColor = 'white'
+            }else if(scrollPosition > scrollThresholdTabletSmall){
+                newColor = 'black'
+            }else{
+                newColor = 'white'
+            }
         }
-        setColor(color);    }, [scrollPosition]);
+        
+        if(deviceType === 'tablet'){
 
+            const scrollThresholdTablet = 64
+            const scrollThresholdTablet2 = 1270
+            const scrollThresholdTablet3 = 3664
+
+
+            if(scrollPosition > scrollThresholdTablet3){
+                newColor = 'black'
+            }else if(scrollPosition > scrollThresholdTablet2){
+                newColor = 'white'
+            }else if(scrollPosition > scrollThresholdTablet){
+                newColor = 'black'
+            }else{
+                newColor = 'white'
+            }
+        }
+
+        if(deviceType === 'notebook'){
+
+            const scrollThresholdNotebook = 67
+            const scrollThresholdNotebook2 = 1471
+            const scrollThresholdNotebook3 = 4264
+
+
+            if(scrollPosition > scrollThresholdNotebook3){
+                newColor = 'black'
+            }else if(scrollPosition > scrollThresholdNotebook2){
+                newColor = 'white'
+            }else if(scrollPosition > scrollThresholdNotebook){
+                newColor = 'black'
+            }else{
+                newColor = 'white'
+            }
+        }
+
+        else {
+            const scrollThreshold = 60;
+            const scrollThreshold2 = 820;
+            const scrollThreshold3 = 2260;
+
+            if (scrollPosition > scrollThreshold3) {
+                newColor = 'black';
+            } else if (scrollPosition > scrollThreshold2) {
+                newColor = 'white';
+            } else if (scrollPosition > scrollThreshold) {
+                newColor = 'black';
+            } else {
+                newColor = 'white';
+            }
+        }
+
+        setColor(newColor);
+    }, [deviceType, scrollPosition]);
 
     return (
         <div className='contactMe pointer'>
-                <a style={{color : color}} href={"mailto:pedrohenriquedevprog@gmail.com"}>Contate-Me</a>
-             </div>
+            <a style={{ color: color }} href={"mailto:pedrohenriquedevprog@gmail.com"}>Contate-Me</a>
+        </div>
     );
 };
 
