b0e63a537ea0df4dde1d6ee014caf523d653c966
SoaresPedronhoDev <pedrohenriquedevprog@gmail.com>
2025-08-12 20:38:45 -0300

    adicionando o projeto To Do ao Portfolio

--- src/components/Projetos/projetos.jsx
+++ src/components/Projetos/projetos.jsx
@@ -20,8 +20,8 @@ function Projetos(){
             <hr/>
             <div className="box-projetos">
                 <img src="/assets/icons/spotify.png" alt="Spotify Clone" />
-                <span className="box-text">Neste projeto, desenvolvi um clone do aplicativo Spotify com o objetivo de aprimora
-                    r minhas habilidades em desenvolvimento de interfaces e, ao mesmo tempo, demonstrar minha capacidade de organização
+                <span className="box-text">Neste projeto, desenvolvi um clone do aplicativo Spotify com o objetivo de aprimorar 
+                    minhas habilidades em desenvolvimento de interfaces e, ao mesmo tempo, demonstrar minha capacidade de organização
                      e estruturação de projetos. O foco principal foi apresentar uma arquitetura limpa, com uma divisão clara de pastas,
                       componentes e recursos, facilitando a manutenção e a escalabilidade do código. Além disso, procurei aplicar boas práticas
                        no tratamento de arquivos e na reutilização de componentes, visando um código mais eficiente, legível e de fácil compreensão
@@ -29,8 +29,15 @@ function Projetos(){
             </div>
             <hr/>
             <div className="box-projetos">
-                <img src="/assets/icons/coming-soon.jpg" alt="Em Breve" />
-                <span className="box-text">Em Breve</span>
+                <img src="/assets/icons/to do image.webp" alt="To Do Image" />
+                <span className="box-text">Desenvolvi esta aplicação para demonstrar minha capacidade de manipular rotas
+                     tanto no front-end quanto no back-end, evidenciando minha experiência full stack.
+                    No front-end, implementei uma interface intuitiva e responsiva, garantindo uma experiência
+                     fluida para o usuário ao criar, editar e organizar suas tarefas. No back-end, construí um sistema
+                      seguro para registro e login, com gerenciamento de sessões e persistência dos dados.
+                    Com este projeto, mostro não apenas minhas habilidades em desenvolvimento de interfaces, mas também meu conhecimento em construção de APIs, segurança
+                     e manipulação eficiente de rotas, provando que sou capaz de atuar de forma integrada em toda a aplicação
+                     .<a href="https://github.com/SoaresPedronhoDev/To-Do-List">Clique aqui para ver o projeto</a></span>
             </div>
         </div>
         <Link className='header-link' to='Header' smooth={true} duration={1000}><p>Voltar ao Topo</p></Link>
